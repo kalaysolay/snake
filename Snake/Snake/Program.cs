b0e7c0d5ae8d0ddce5a7bc3b7121999e43aee9d6
kalaysolay <ashat_boztaev@Mail.ru>
2015-10-09 11:24:47 +0600

    #lesson3

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Program
-    {
-        static void Main(string[] args) {
-            PrintSymbol(1, 5, '*');
+    internal class Program {
+        private static void Main( string[] args ) {
+            var p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+
+            p1.PrintSymbol();
+
+            var p2 = new Point();
+            p2.x = 3;
+            p2.y = 7;
+            p2.sym = '$';
+            p2.PrintSymbol();
+            Console.ReadKey();
+
+            /*      PrintSymbol(1, 5, '*');
             PrintSymbol(2, 6, '*');
             PrintSymbol(3, 7, '*');
 
             Console.ReadKey();
+        }*/
+
+
         }
 
-        public static void PrintSymbol( int x, int y, char s ) {
-         Console.SetCursorPosition( x,y );
-         Console.Write(s);
-        }
+
     }
 }
